757bbcc0e05258b21acf5aa4b0b08185dffb907c
Manuel-aguilera <Manueleitor.information@gmail.com>
2020-08-20 08:17:58 -0500

    End of tests

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -1,11 +1,13 @@
 //  Uncomment if using Node
-// var chai = require('chai');
-var assert = chai.assert;
+var chai = require("chai");
+const assert = chai.assert;
 
-describe('Array', function() {
-  it('should start empty', function() {
-    var arr = [];
+describe("Array", function () {
+	it("should start empty", function () {
+		//test implementation goes here
+		const arr = [];
+		assert.equal(arr.length, 0, "Array length was not 0");
+	});
 
-    assert.equal(arr.length, 0);
-  });
+	//We can have more its here
 });
--- test/classNameTest.js
+++ test/classNameTest.js
@@ -1,34 +1,32 @@
 // Uncomment if using Node
-// var chai = require('chai');
+var chai = require("chai");
 var assert = chai.assert;
 
-// var className = require('../js/className.js');
-// var addClass = className.addClass;
+var className = require("../js/className");
+var addClass = className.addClass;
 
-describe('addClass', function() {
-  it('should add class into element', function() {
-    var element = { className: '' };
+describe("addClass", function () {
+	it("should add class into element", function () {
+		let element = { className: "" };
 
-    addClass(element, 'test-class');
+		addClass(element, "test-class");
 
-    assert.equal(element.className, 'test-class');
-  });
+		assert.equal(element.className, "test-class");
+	});
+	it("should not add a class which already exists", function () {
+		let element = { className: "exists" };
 
-  it('should not add a class which already exists in element', function() {
-    var element = { className: 'exists' };
+		addClass(element, "exists");
 
-    addClass(element, 'exists');
+		let numClasses = element.className.split(" ").length;
+		assert.equal(numClasses, 1);
+	});
+	it("should append new class after existing one", function () {
+		let element = { className: "exists" };
 
-    var numClasses = element.className.split(' ').length;
-    assert.equal(numClasses, 1);
-  });
+		addClass(element, "new-class");
 
-  it('should append new class after existing one', function() {
-    var element = { className: 'exists' };
-
-    addClass(element, 'new-class');
-
-    var classes = element.className.split(' ');
-    assert.equal(classes[1], 'new-class');
-  });
+		const classes = element.className.split(" ");
+		assert.equal(classes[1], "new-class");
+	});
 });
